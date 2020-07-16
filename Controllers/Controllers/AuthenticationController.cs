2c0dc330b37a51bbfdee5bb83b63cbf3ccc1c909
Pasto <dpastierovic@gmail.com>
2020-07-16 23:43:40 +0200

    Added Authenticate

--- Controllers/Controllers/AuthenticationController.cs
+++ Controllers/Controllers/AuthenticationController.cs
@@ -1,30 +1,51 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Controllers.Utilities;
 using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Controllers.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("/api/[controller]")]
     public class AuthenticationController : ControllerBase
     {
         private readonly IConfiguration _configuration;
+        private readonly IHttpClientFactory _clientFactory;
 
-        public AuthenticationController(IConfiguration configuration)
+        public AuthenticationController(IConfiguration configuration, IHttpClientFactory clientFactory)
         {
             _configuration = configuration;
+            _clientFactory = clientFactory;
         }
 
-        [HttpGet]
-        public string Get()
+        [HttpPut]
+        [Consumes("text/plain")]
+        [Route("/api/[controller]/code")]
+        public async Task<IActionResult> PutToken()
         {
-            var urlBuilder = new UrlBuilder(_configuration["StravaApiUrl"]);
-            urlBuilder.AddQueryParameter("response_type", "code");
-            urlBuilder.AddQueryParameter("client_id", _configuration["ClientId"]);
-            urlBuilder.AddQueryParameter("redirect_uri", _configuration["ClientUrl"]);
-            urlBuilder.AddQueryParameter("scope", "activity:write,read");
-            urlBuilder.AddQueryParameter("approval_prompt", "auto");
-            return urlBuilder.Build();
+            using var reader = new StreamReader(Request.Body, Encoding.UTF8);
+            var code = await reader.ReadToEndAsync();
+
+            var client = _clientFactory.CreateClient();
+
+            var authRequest = new HttpRequestMessage(HttpMethod.Post, _configuration["StravaOauthTokenUrl"])
+            {
+                Content = new FormUrlEncodedContent(new Dictionary<string, string>
+                {
+                    {"client_id", _configuration["ClientId"]},
+                    {"client_secret", _configuration["ClientSecret"]},
+                    {"code", code},
+                    {"grant_type", "authorization_code"}
+                })
+            };
+            var response = JsonConvert.DeserializeObject<Dictionary<string, object>>(await (await client.SendAsync(authRequest)).Content
+                .ReadAsStringAsync());
+
+            return Ok($"{{\"access_token\": \"{response["access_token"]}\"}}");
         }
     }
 }
